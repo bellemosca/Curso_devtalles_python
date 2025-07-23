33295a53b8b0e14f45c2f75db6580d7f109066e3
Isabelle Mosca <bellemosca@gmail.com>
2025-07-22 21:55:03 -0300

    Refactor tuple examples and add set examples in new scripts

---
+++ 6 - Tuplas/01-tuplas.py
@@ -0,0 +1,21 @@
+# Imutável
+# Ordenada
+# Permite duplicadas
+# São indexadas
+
+minha_tupla = (1, 2, 3, 4, "oi", True, 2, "Hi", 5, 4, 3, 2)
+# print(minha_tupla)
+
+# Métodos
+# .count()
+# print(minha_tupla.count(2))
+
+# .index()
+print(minha_tupla.index("oi"))
+
+# minha_tupla[4] = "Mundo"   Não pode fazer isso!
+nova_tupla = minha_tupla[4]
+print(nova_tupla)
+
+semana = ("segunda", "terça", "quarta", "quinta", "sexta", "sábado", "domingo")
+# exemplo de dados que são imutáveis e podem ser usados com tupla
---
+++ 6 - Tuplas/02-setes.py
@@ -0,0 +1,11 @@
+my_set = {1, 2, 3, 5, 4, 3, 2, 1, 3, 9, 7, 3, 8}
+
+# Coleção desordenada
+# Não tem indices
+# Não permite duplicatas
+# Ótimo para operações matematicas
+
+print(my_set)
+
+user_name = {"isabelle123", "felipe456", "devi", "devi"}
+print(user_name)
