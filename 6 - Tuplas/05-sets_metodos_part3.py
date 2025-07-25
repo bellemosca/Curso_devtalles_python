e06d85c2fd6a1b8a5bf8971f9a267fab9d88c9cd
Isabelle Mosca <bellemosca@gmail.com>
2025-07-24 21:54:29 -0300

    Add examples for set subset and superset methods; remove unused tuple script

--- 6 - Tuplas/05-sets_metodos_part3.py
+++ 6 - Tuplas/05-sets_metodos_part3.py
@@ -1,6 +1,18 @@
+# Serve para comparar CONJUNTOS de dados!
+
 # set1.symmetric_difference(set2)
 set1 = {1, 2, 3, 4}
 set2 = {3, 4, 5, 6}
 
 symmetric_difference = set1.symmetric_difference(set2)
-print(symmetric_difference)
+# print(symmetric_difference)
+
+# set1.issubset(set2) True ou False
+set1 = {1, 2}
+set2 = {1, 2, 3, 4}
+# print(set1.issubset(set2))
+
+# set1.issuperset(set2)
+set1 = {1, 2, 3, 4}
+set2 = {1, 2}
+print(set2.issuperset(set1))
