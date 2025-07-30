f103255164d788f2f9876a686eca97ab9cfa8990
Isabelle Mosca <bellemosca@gmail.com>
2025-07-29 21:57:00 -0300

    Remove unused tuple script and add conversion examples for list to tuple and set

---
+++ 6 - Tuplas/07-converts.py
@@ -0,0 +1,11 @@
+list1 = [1, 2, 3, 4, 5, 6, 2, 3, 7, 8, 1, 3, 9, 8, 10]
+tuple1 = tuple(list1)
+# print(list1)
+# print(tuple1)
+
+set1 = set(list1)  # ele filtra os dados da lista, excluindo os que estão repetidos
+# print(set1)
+
+list_tuple = [("a", 1), ("b", 2), ("c", 3)]
+dictionary = dict(list_tuple)
+print(dictionary)
