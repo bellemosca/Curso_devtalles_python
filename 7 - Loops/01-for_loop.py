403852b39802dae0543e1ebcd396fcfa4fadd327
Isabelle Mosca <bellemosca@gmail.com>
2025-07-30 21:49:26 -0300

    Remove unused tuple script and add initial for loop examples

---
+++ 7 - Loops/01-for_loop.py
@@ -0,0 +1,14 @@
+# for i in "Pyhton":
+#    print(i)
+
+frutas = ["Maça", "Laranja", "Banana"]
+
+# for fruta in frutas:
+#    print(fruta)
+
+
+lista = [1, 2, 3, 4, 5]
+sum = 0
+for n in lista:
+    sum += n
+    print(sum)
