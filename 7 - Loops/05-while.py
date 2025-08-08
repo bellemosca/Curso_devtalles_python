079b780f58e76a76638a2d2edf5548cd44d0d7fe
Isabelle Mosca <bellemosca@gmail.com>
2025-08-08 16:14:29 -0300

    Remove unused tuple script and add while loop examples

---
+++ 7 - Loops/05-while.py
@@ -0,0 +1,17 @@
+# while True:
+#  print("Esse nunca se acabará.") # loop infinito
+
+contador = 1
+while contador <= 5:
+    print(f"Numbero: {contador}")
+    contador += 1
+else:
+    ("Terminamos")
+
+
+resposta = " "
+
+while resposta.lower() != "Python":
+    resposta = input("Escreva Python para sair: ")
+
+print("Terminamos")
