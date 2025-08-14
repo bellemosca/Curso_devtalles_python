024493718182e9a84bc9d8503f621faaf57bae24
Isabelle Mosca <bellemosca@gmail.com>
2025-08-13 21:02:21 -0300

    Remove unused tuple script and add examples for break, continue, and pass in loops

---
+++ 7 - Loops/06-break_continue_pass.py
@@ -0,0 +1,16 @@
+# Pass
+for item in [1, 2, 3, 4, 5]:
+    pass  # ele 'pausa' o código, deixando ele inutilizado
+
+# Break
+# for item in [1, 2, 3, 4, 5]:
+#    if item == 4:
+#        break
+#    print(item)
+
+# Continue
+num = 0
+while num < len([1, 2, 3, 4, 5]):
+    num += 1
+    continue
+    print(num)
