9a2143d73871e4cc531c435cf0211da629643cf4
Isabelle Mosca <bellemosca@gmail.com>
2025-08-14 14:24:27 -0300

    Remove unused tuple script and update loop exercise to include an option to exit the program

--- 7 - Loops/10-exercicio_loops.py
+++ 7 - Loops/10-exercicio_loops.py
@@ -6,6 +6,7 @@ print(
       4 - Buscar produto
       5 - Contar produtos
       6 - Esvaziar carrinho
+      7 - Encerrar programa
 """
 )
 
@@ -21,41 +22,46 @@ carrinho = [
     "Farinha de trigo",
     "Sabonete",
 ]
-opcoes = int(input("Escolha uma opção de 1 a 6: "))
 
-if opcoes == 1:
-    produto = input("Adicionar produto: ")
-    if produto not in carrinho:
-        carrinho.append(produto)
-        print("Produto adicionado com sucesso!")
-    else:
-        print("Produto já existente. Adicione outro.")
-elif opcoes == 2:
-    remover = input("Qual item você deseja remover? ")
-    if remover in carrinho:
-        carrinho.remove(remover)
-        print("Produto removido.")
-    else:
-        print("Produto não existente.")
-elif opcoes == 3:
-    if len(carrinho) > 0:
-        print("Lista de compras: ")
-        carrinho.sort()
-        print(carrinho)
-    else:
-        print("A lista está vazia.")
-elif opcoes == 4:
-    busca_prod = input("Digite o produto que está buscando: ")
-    if busca_prod in carrinho:
-        print(f"{busca_prod} está na lista.")
-    else:
-        print("Este produto não está na lista")
-elif opcoes == 5:
-    print(f"Total de produtos no carrinho: {len(carrinho)}")
-elif opcoes == 6:
-    carrinho.clear()
-    print("Carrinho esvaziado com sucesso.")
-else:
-    print("Opção inválida.")
+while True:
+    opcoes = int(input("Escolha uma opção de 1 a 6: "))
 
-print(carrinho)
+    if opcoes == 1:
+        produto = input("Adicionar produto: ")
+        if produto not in carrinho:
+            carrinho.append(produto)
+            print("Produto adicionado com sucesso!")
+        else:
+            print("Produto já existente. Adicione outro.")
+    elif opcoes == 2:
+        remover = input("Qual item você deseja remover? ")
+        if remover in carrinho:
+            carrinho.remove(remover)
+            print("Produto removido.")
+        else:
+            print("Produto não existente.")
+    elif opcoes == 3:
+        if len(carrinho) > 0:
+            print("Lista de compras: ")
+            carrinho.sort()
+            print(carrinho)
+        else:
+            print("A lista está vazia.")
+    elif opcoes == 4:
+        busca_prod = input("Digite o produto que está buscando: ")
+        if busca_prod in carrinho:
+            print(f"{busca_prod} está na lista.")
+        else:
+            print("Este produto não está na lista")
+    elif opcoes == 5:
+        print(f"Total de produtos no carrinho: {len(carrinho)}")
+    elif opcoes == 6:
+        carrinho.clear()
+        print("Carrinho esvaziado com sucesso.")
+    elif opcoes == 7:
+        print("Encerrando o programa.")
+        break
+    else:
+        print("Opção inválida.")
+
+    print(carrinho)
