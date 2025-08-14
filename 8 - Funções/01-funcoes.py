862cbd3ccd328490f24b7204869094ad8daca19e
Isabelle Mosca <bellemosca@gmail.com>
2025-08-14 16:51:27 -0300

    Remove unused tuple script and add functions for greeting and farewell

--- 8 - Funções/01-funcoes.py
+++ 8 - Funções/01-funcoes.py
@@ -2,4 +2,8 @@ def hello():
     print("Oi mundo das funções!")
 
 
+def bye():
+    print("Adeus mundo das funções!")
+
+
 print(hello())
---
+++ 8 - Funções/04-default_argumentos.py
@@ -0,0 +1,6 @@
+def hello(greet="Oi", name="Convidado"):
+    print(f"{greet}, {name}")
+
+
+# Keyword arguments
+hello(name="Isabelle", greet="Oi")
