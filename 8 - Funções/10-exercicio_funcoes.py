797c91f774939b03a6966f317e4474dcb869c1f0
Isabelle Mosca <bellemosca@gmail.com>
2025-08-18 22:01:44 -0300

    Add password generator function skeleton with comments for implementation

---
+++ 8 - Funções/10-exercicio_funcoes.py
@@ -0,0 +1,12 @@
+# Criar uma senha com a função
+# letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# numeros = "0123456789"
+# simbolos = "!?@#$%&*()+-=_|\/<>,.;:[]{}"
+# formula simples = (item * 7 + 3) % len(caracteres)
+
+# Entrada: 8
+# Saida: @*(123Ha
+
+
+def password_generator():
+    pass
