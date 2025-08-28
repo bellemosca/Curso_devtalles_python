b88ac58e943774fb62f39aeb366fef3243b17f52
Isabelle Mosca <bellemosca@gmail.com>
2025-08-27 21:55:54 -0300

    Remove tuple example and enhance Person class with static method for age verification

--- 9 - POO/06-class_static_metodos.py
+++ 9 - POO/06-class_static_metodos.py
@@ -1,4 +1,6 @@
 class Person:
+    especies = "Homosapiens"
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -7,11 +9,20 @@ class Person:
     def change_especies(cls, new_especies):
         cls.especies = new_especies
 
+    @staticmethod
+    def is_older(age):
+        return age >= 18
 
-pessoa1 = Person("Isabelle", 33)
+
+"""pessoa1 = Person("Isabelle", 33)
 print(pessoa1.especies)
 Person.change_especies("Homosapiens")
 print(pessoa1.especies)
 
 pessoa2 = Person("Fernando", 20)
-print(pessoa2.especies)
+print(pessoa2.especies)"""
+
+print(Person.is_older(16))
+
+pessoa1 = Person("Isabelle", 33)
+print(Person.is_older(pessoa1.age))
