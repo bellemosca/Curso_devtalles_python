ef12a90f9151f0bc2e63657d4ce8f02e58e766d5
Isabelle Mosca <bellemosca@gmail.com>
2025-08-27 21:46:59 -0300

    Remove tuple example and add Person class with class method for species management

---
+++ 9 - POO/06-class_static_metodos.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def change_especies(cls, new_especies):
+        cls.especies = new_especies
+
+
+pessoa1 = Person("Isabelle", 33)
+print(pessoa1.especies)
+Person.change_especies("Homosapiens")
+print(pessoa1.especies)
+
+pessoa2 = Person("Fernando", 20)
+print(pessoa2.especies)
