714659359fb0040319519e5fc44d23530deaff49
Isabelle Mosca <bellemosca@gmail.com>
2025-10-02 15:02:31 -0300

    Remove tuple example and create abstraction module for future enhancements

---
+++ 10 - POO_2/01-abstraction.py
@@ -0,0 +1,12 @@
+class CoffeMaker:
+    def make_coffe(self):
+        self.__boil_water()
+        self.__mix()
+        print("PIP PIP")
+        print("O café esta pronto")
+
+    def __boil_water(self):
+        pass
+
+    def __mix(self):
+        pass
